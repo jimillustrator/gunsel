11e3f296e7eda97b2028ddcf6664aa52eb8c788b
Jim McNeill <jimillustrator@gmail.com>
2023-07-28 15:18:32 -0700

    Copied and pasted GameDev's enemyAI script because mine was producing a complier error I couldn't figure out

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -9,27 +9,53 @@ public class EnemyAI : MonoBehaviour
     [SerializeField] float chaseRange = 5f;
 
     NavMeshAgent navMeshAgent;
-
     float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
 
     void Start()
     {
-        navMeshAgent = GetComponent<NavMeshAgent>(); 
+        navMeshAgent = GetComponent<NavMeshAgent>();
     }
-    
+
     void Update()
     {
         distanceToTarget = Vector3.Distance(target.position, transform.position);
-
-        if (distanceToTarget <= chaseRange)
-        { 
-        navMeshAgent.SetDestination(target.position);
+        if (isProvoked)
+        {
+            EngageTarget();
         }
+        else if (distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+        }
+    }
+
+    private void EngageTarget()
+    {
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+
+        if (distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void AttackTarget()
+    {
+        Debug.Log(name + " has seeked and is destroying " + target.name);
     }
 
     void OnDrawGizmosSelected()
     {
-       Gizmos.color = Color.magenta;
-       Gizmos.DrawWireSphere(transform.position, chaseRange);
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
 }
