3832393629da9c0f54c81e33835c33593f2ca729
Jim McNeill <jimillustrator@gmail.com>
2023-08-12 15:27:11 -0700

    Added extremely primitive Muzzle Flash to Player Revolver

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -7,6 +7,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 30f;
+    [SerializeField] ParticleSystem muzzleFlash;
 
     void Update()
     {
@@ -18,8 +19,20 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRaycast()
+    {
         RaycastHit hit;
-        if(Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
             Debug.Log("I hit this thing: " + hit.transform.name);
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
@@ -30,6 +43,5 @@ public class Weapon : MonoBehaviour
         {
             return;
         }
-        
     }
 }
