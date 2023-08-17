e6aa5041f8d1ab02871cce89b175242dae031e36
Jim McNeill <jimillustrator@gmail.com>
2023-08-17 15:25:51 -0700

    Added Enemy Rotation so it tracks the Player

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -7,6 +7,7 @@ public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
     [SerializeField] float chaseRange = 5f;
+    [SerializeField] float turnSpeed = 5f;
 
     NavMeshAgent navMeshAgent;
     float distanceToTarget = Mathf.Infinity;
@@ -32,6 +33,7 @@ public class EnemyAI : MonoBehaviour
 
     private void EngageTarget()
     {
+        FaceTarget();
         if (distanceToTarget >= navMeshAgent.stoppingDistance)
         {
             ChaseTarget();
@@ -54,6 +56,19 @@ public class EnemyAI : MonoBehaviour
     {
         GetComponent<Animator>().SetBool("attack", true);
 
+    }
+
+    private void FaceTarget()
+    {
+        //the subtraction below establishes the vector
+        //the .normalized below eliminates the magitude (distance) of the vector since we only
+        //want the angle of rotation
+
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
+
+        //Slerp stands for Spherical Interpolation(smooth rotation)
     }
 
     void OnDrawGizmosSelected()
