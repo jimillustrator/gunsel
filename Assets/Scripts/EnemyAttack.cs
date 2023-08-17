70af8d83acbec5358cf3e3347f21ff556af19eeb
Jim McNeill <jimillustrator@gmail.com>
2023-08-17 14:52:22 -0700

    Added Player Damage to Enemy Attack Script

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-    //Variable of transform type to indicate where the enemy attack is going to happen
-    [SerializeField] Transform target;
+    //Variable of PlayerHealth type because the C# script PlayerHealth created the type
+    PlayerHealth target;
     [SerializeField] float damage = 40f;
 
     void Start()
     {
-        
+        //Okay to put "FindObjectOfType" in Start() but not in Update()
+        target = FindObjectOfType<PlayerHealth>(); 
     }
 
     public void AttackHitEvent()
     {
         if (target == null) return;
+        //target already has TakeDamage component because it was initialized in Start()
+        target.TakeDamage(damage);
         Debug.Log("bang bang");
     }
 
