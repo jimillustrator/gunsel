9f94e883401ce2c4ce6470deb95097e8d257eb8c
Jim McNeill <jimillustrator@gmail.com>
2023-08-19 15:11:43 -0700

    Added Death Handler Script and enabled the cursor to get locked to the view after the game restarts

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -7,7 +7,10 @@ public class SceneLoader : MonoBehaviour
 {
     public void ReloadGame()
     {
-        SceneManager.LoadScene(0);  
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     public void QuitGame()
