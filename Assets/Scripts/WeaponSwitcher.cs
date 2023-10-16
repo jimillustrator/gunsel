33f25b181ed874883adbcf9418e9c518b15216e8
Jim McNeill <jimillustrator@gmail.com>
2023-10-16 13:14:42 -0700

    Added Weapon Switcher script to Weapon Empty (parent object for weapons in the hierarchy)

---
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitcher : MonoBehaviour
+{
+    //This script is being attached to the Weapons Empty,
+    //not to the individual weapons or the Player
+
+    [SerializeField] int currentWeapon = 0;
+
+    void Start()
+    {
+        SetWeaponActive();
+    }
+
+    void SetWeaponActive()
+    {
+        int weaponIndex = 0;
+
+        foreach (Transform weapon in transform)
+        {
+            if (weaponIndex == currentWeapon)
+            {
+                weapon.gameObject.SetActive(true);
+            }
+            else
+            {
+                weapon.gameObject.SetActive(false);
+            }
+            weaponIndex++;
+        }
+    }
+}
