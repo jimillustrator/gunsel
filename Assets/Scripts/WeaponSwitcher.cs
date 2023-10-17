52ee2d9e63b3eb0063475ee09578acf4d8d21105
Jim McNeill <jimillustrator@gmail.com>
2023-10-17 08:30:25 -0700

    Implemented weapon switching functionality with the scroll wheel and keyboard numbers

--- Assets/Scripts/WeaponSwitcher.cs
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -15,6 +15,72 @@ public class WeaponSwitcher : MonoBehaviour
         SetWeaponActive();
     }
 
+    void Update()
+    {
+        //This establishes the baseline with the weapon the game starts with 
+        int previousWeapon = currentWeapon;
+
+        //These two change the current weapon for the following if statement to evaluate
+        ProcessKeyInput();
+        ProcessScrollWheeel();
+
+        if(previousWeapon != currentWeapon)
+        {
+            SetWeaponActive();
+        }
+    }
+
+    void ProcessKeyInput()
+    {
+        //Alpha1 is the keycode for the 1 key on the keyboard
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentWeapon = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentWeapon = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            currentWeapon = 2;
+        }
+    }
+
+    void ProcessScrollWheeel()
+    {
+        //Mouse ScrollWheel is a string reference that we got from the Edit>
+        //Project Settings>Input Manager>Mouse ScrollWheel in Unity iself
+
+        if(Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            //the "-1" is to convert it to index number
+            //thetransform is the trasform of the weapons empty, since this script is on that
+            if(currentWeapon >= transform.childCount-1)
+            {
+                currentWeapon = 0;
+            }
+            else
+            {
+                currentWeapon++;
+            }
+        }
+
+        if(Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if(currentWeapon <= 0)
+            {
+                currentWeapon = transform.childCount-1;
+            }
+            else
+            {
+                currentWeapon--;
+            }
+        }
+    }
+
+
+
     void SetWeaponActive()
     {
         int weaponIndex = 0;
